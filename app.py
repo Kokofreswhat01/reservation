29d7575b74e939753ba69638536a60ca323de54c
Kokofreswhat01 <101075714+Kokofreswhat01@users.noreply.github.com>
2023-06-12 17:20:48 +0000

    ok

--- app.py
+++ app.py
@@ -1,33 +1,67 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 from flask_sqlalchemy import SQLAlchemy
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'  # Configuration de la base de données SQLite
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite3://base.db'  # Configuration de la base de données SQLite
+db = SQLAlchemy()
 
 
-@app.route('/')
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.email
+
+
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
-
-@app.route('/connexion')
+    
+@app.route('/connexion', methods=['POST', 'GET'])
 def connexion():
-    email = request.form.get('email')
-    password = request.form.get('password')
+    if request.method == 'POST':
+        # Récupérer les valeurs du formulaire
+        email = request.form.get('email')
+        password = request.form.get('mot_de_passe')
 
-    user = User.query.filter_by(email=email).first()
-    if user and user.password == password:
-        return "Connexion réussie pour l'utilisateur : {}".format(email)
-    else:
-        return "Email ou mot de passe incorrect.", 401
+        # Vérifier les informations de connexion
+        user = User.query.filter_by(email=email).first()
+
+        # Si les informations sont valides, rediriger vers la page d'accueil
+        if user and user.password == password:
+            return render_template('reservation_voit.html')
+        return render_template('connexion.html')
+    return render_template('index.html')
 
 
-@app.route('/inscription')
+@app.route('/inscription', methods=['POST', 'GET'])
 def inscription():
+    if request.method == 'POST':
+        name = request.form.get('Nom & Prenom')
+        email = request.form.get('email')
+        password = request.form.get('mot_de_passe')
+        new_user = User(user=name, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return render_template('connexion.html')
     return render_template('inscription.html')
 
-@app.route('/contact')
+
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        numero = request.form.get('numero')
+        objet = request.form.get('objet')
+        message = request.form.get('message')
+
+        # Traitez les données du formulaire de contact, par exemple, envoyez un e-mail, enregistrez-les dans une base de données, etc.
+
+        thank_you_message = f"Merci {name} pour votre message. Nous vous contacterons bientôt."
+        return render_template('contact.html', thank_you_message=thank_you_message)
+    
     return render_template('contact.html')
 
 @app.route('/connexion/reservation_taxi')
@@ -41,30 +75,3 @@ def reservation_voit():
 if __name__ == "__main__":
     db.create_all()
     app.run
-
-
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.email
-
-@app.route('/connexion', methods=['POST'])
-def connexion():
-    email = request.form.get('email')
-    password = request.form.get('password')
-
-    user = User.query.filter_by(email=email).first()
-
-    if user and user.password == password:
-        return "Connexion réussie pour l'utilisateur : {}".format(email)
-    else:
-        return "Email ou mot de passe incorrect.", 401
-
-if __name__ == '__main__':
-    
-    app.run()
