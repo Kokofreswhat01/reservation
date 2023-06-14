45bccd2f5a6a54bfd288363eccc8b3ae4e8830cd
Kokofreswhat01 <101075714+Kokofreswhat01@users.noreply.github.com>
2023-06-14 17:07:21 +0000

    ok

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite3://base.db'  # Configuration de la base de données SQLite
