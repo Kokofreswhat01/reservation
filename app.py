3036bf7ec1d1fd45ac306032ddb4ee25d5ad78e4
Kokofreswhat01 <101075714+Kokofreswhat01@users.noreply.github.com>
2023-06-16 17:13:54 +0000

    modification nouvelle

--- app.py
+++ app.py
@@ -1,25 +1,29 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite3://base.db'  # Configuration de la base de données SQLite
 db = SQLAlchemy()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'  # Configuration de la base de données SQLite
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), unique=False, nullable=False)
     password = db.Column(db.String(120), nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.email
 
 
-@app.route('/index', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
-    
-@app.route('/connexion', methods=['POST', 'GET'])
-def connexion():
+
+
+@app.route('/coon', methods=['POST', 'GET'])
+def coon():
     if request.method == 'POST':
         # Récupérer les valeurs du formulaire
         email = request.form.get('email')
@@ -30,26 +34,32 @@ def connexion():
 
         # Si les informations sont valides, rediriger vers la page d'accueil
         if user and user.password == password:
-            return render_template('reservation_voit.html')
-        return render_template('connexion.html')
-    return render_template('index.html')
+            return render_template('HTML/taxi.html')
+    return render_template('HTML/coon.html')
+    #return render_template('index.html')
 
 
-@app.route('/inscription', methods=['POST', 'GET'])
-def inscription():
+@app.route('/inscri', methods=['POST', 'GET'])
+def inscri():
     if request.method == 'POST':
-        name = request.form.get('Nom & Prenom')
+        name = request.form.get('nom')
         email = request.form.get('email')
         password = request.form.get('mot_de_passe')
-        new_user = User(user=name, email=email, password=password)
+        new_user = User(name=name, email=email, password=password)
         db.session.add(new_user)
         db.session.commit()
-        return render_template('connexion.html')
-    return render_template('inscription.html')
+        return render_template('HTML/coon.html')
+    return render_template('HTML/inscri.html')
+
+@app.route('/logout')
+def logout():
+    # Supprimez l'utilisateur de la session pour le déconnecter
+    db.session.pop('username', None)
+    return redirect(url_for('coon'))
 
 
-@app.route('/contact', methods=['GET', 'POST'])
-def contact():
+@app.route('/contt', methods=['GET', 'POST'])
+def contt():
     if request.method == 'POST':
         name = request.form.get('name')
         email = request.form.get('email')
@@ -60,18 +70,22 @@ def contact():
         # Traitez les données du formulaire de contact, par exemple, envoyez un e-mail, enregistrez-les dans une base de données, etc.
 
         thank_you_message = f"Merci {name} pour votre message. Nous vous contacterons bientôt."
-        return render_template('contact.html', thank_you_message=thank_you_message)
-    
-    return render_template('contact.html')
+        return render_template('HTML/contt.html', thank_you_message=thank_you_message)
 
-@app.route('/connexion/reservation_taxi')
-def reservation_taxi():
-    return render_template('reservation_taxi.html')
+    return render_template('HTML/contt.html')
 
-@app.route('/connexion/reservation_taxi/reservation_voit')
-def reservation_voit():
-    return render_template('reservation_voit.html')
 
+@app.route('/coon/taxi')
+def taxi():
+    return render_template('HTML/taxi.html')
+
+
+@app.route('/coon/taxi/voit_car')
+def voit_car():
+    return render_template('HTML/voit_car.html')
+
+
+db.init_app(app)
 if __name__ == "__main__":
     db.create_all()
-    app.run
+    app.run()
